7f18e4cf91d11e9e4b36f7cfc2d48ab9a1d71f68
vladlengg <ocvbjf5b24aq4snhfnbtlgzuvf4s6vdeygk72cjuxxap3s46o6vq>
2022-04-28 09:22:51 +0500

    change home controller for admin

--- src/app/http/controllers/staff/HomeController.ts
+++ src/app/http/controllers/staff/HomeController.ts
@@ -7,7 +7,41 @@ import {Request, Response} from "express"
 
 export default {
     getStatistic: async (req: Request, res: Response) => {
-        const {dateFrom, dateTo} = req.body
+        const {dateFrom, dateTo, type} = req.body
+        let momentFrom = moment().startOf("day").format("YYYY-MM-DD HH:mm:ss")
+        let momentTo = moment().endOf("day").format("YYYY-MM-DD HH:mm:ss")
+
+        if (type === "custom") {
+            momentFrom = moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss")
+            momentTo = moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+        } else {
+            if (type === "today") {
+                momentFrom = moment().startOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentTo = moment().endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            } else if (type === "yesterday") {
+                momentFrom = moment().subtract(1, "days").startOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentTo = moment().subtract(1, "days").endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            } else if (type === "7d") {
+                momentFrom = moment().subtract(7, "days").startOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentTo = moment().endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            } else if (type === "30d") {
+                momentFrom = moment().subtract(30, "days").startOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentTo = moment().endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            } else if (type === "3m") {
+                momentFrom = moment().subtract(3, "months").startOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentTo = moment().endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            } else if (type === "3m") {
+                momentFrom = moment().subtract(3, "months").startOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentTo = moment().endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            } else if (type === "6m") {
+                momentFrom = moment().subtract(6, "months").startOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentTo = moment().endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            } else if (type === "12m") {
+                momentFrom = moment().subtract(12, "months").startOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentTo = moment().endOf("day").format("YYYY-MM-DD HH:mm:ss")
+            }
+        }
+
         const response = {
             revenue: 0,
             costs: 0,
@@ -16,6 +50,7 @@ export default {
             numberOfOnlineOrders: 0,
             numberOfNewClients: 0,
             averageCheck: 0,
+            dateFormat: "MMM YY",
             revenueByDay: []
         }
 
@@ -23,13 +58,11 @@ export default {
             .where(builder => {
                 builder.whereNull("status_id")
                     .orWhere("status_id", 16)
-            })
-            .sum({total: "total_price"})
-
+            }).sum({total: "total_price"})
         if (dateFrom && dateTo)
             revenue.whereBetween("created_at", [
-                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentFrom,
+                momentTo
             ])
 
         response.revenue = (await revenue)[0]?.total || 0
@@ -42,14 +75,14 @@ export default {
             .whereIn("source_id", [5, 6])
         if (dateFrom && dateTo)
             numberOfChecks.whereBetween("created_at", [
-                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentFrom,
+                momentTo
             ])
 
         response.numberOfChecks = (await numberOfChecks)[0]?.total || 0
 
 
-        const numberOfOnlineOrders = Order.query<any>()
+        const numberOfOnlineOrders = Order.query()
             .where(builder => {
                 builder.whereNull("status_id")
                     .orWhere("status_id", 16)
@@ -57,8 +90,8 @@ export default {
             .whereNotIn("source_id", [5, 6])
         if (dateFrom && dateTo)
             numberOfOnlineOrders.whereBetween("created_at", [
-                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentFrom,
+                momentTo
             ])
 
         response.numberOfOnlineOrders = (await numberOfOnlineOrders)[0]?.total || 0
@@ -68,13 +101,11 @@ export default {
             .where(builder => {
                 builder.whereNull("status_id")
                     .orWhere("status_id", 16)
-            })
-            .avg({total: "total_price"})
-
+            }).avg({total: "total_price"})
         if (dateFrom && dateTo)
             averageCheck.whereBetween("created_at", [
-                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentFrom,
+                momentTo
             ])
 
         response.averageCheck = (await averageCheck)[0]?.total || 0
@@ -84,41 +115,104 @@ export default {
             .count("*", {as: "total"})
         if (dateFrom && dateTo)
             numberOfNewClients.whereBetween("created_at", [
-                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
+                momentFrom,
+                momentTo
             ])
 
         response.numberOfNewClients = (await numberOfNewClients)[0]?.total || 0
 
 
-        const revenueByDay = Order.query<any>()
-            .select(raw(`DATE(created_at) AS date`))
-            .sum({total: "total_price"})
-            .groupBy("date")
-            .where(builder => {
-                builder.whereNull("status_id")
-                    .orWhere("status_id", 16)
-            })
-        if (dateFrom && dateTo)
-            revenueByDay.whereBetween("created_at", [
-                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
-            ])
+        const duration = moment.duration(moment(momentTo).diff(momentFrom))
+        const hours = duration.asHours()
+        const months = duration.asMonths()
 
-        response.revenueByDay = await revenueByDay
+        if (hours <= 24) {
+            const revenueByDay = Order.query<any>()
+                .select(raw(`DATE_FORMAT(created_at,'%Y-%m-%d %H') AS date`))
+                .sum({total: "total_price"})
+                .groupBy("date")
+                .where(builder => {
+                    builder.whereNull("status_id")
+                        .orWhere("status_id", 16)
+                })
+            if (dateFrom && dateTo)
+                revenueByDay.whereBetween("created_at", [
+                    momentFrom,
+                    momentTo
+                ])
+
+            response.revenueByDay = await revenueByDay
+            response.dateFormat = "HH ч. DD MMM"
 
 
-        const numberOfPositions = OrderProductColor.query<any>()
-            .count("*", {as: "total"})
+            const numberOfPositions = OrderProductColor.query<any>()
+                .count("*", {as: "total"})
 
-        if (dateFrom && dateTo)
-            numberOfPositions.whereBetween("created_at", [
-                moment(dateFrom).startOf("day").format("YYYY-MM-DD HH:mm:ss"),
-                moment(dateTo).endOf("day").format("YYYY-MM-DD HH:mm:ss")
-            ])
+            if (dateFrom && dateTo)
+                numberOfPositions.whereBetween("created_at", [
+                    momentFrom,
+                    momentTo
+                ])
 
-        response.numberOfPositions = (await numberOfPositions)[0]?.total || 0
+            response.numberOfPositions = (await numberOfPositions)[0]?.total || 0
+        } else if (months >= 3) {
+            const revenueByDay = Order.query<any>()
+                .select(raw(`DATE_FORMAT(created_at,'%Y-%m') AS date`))
+                .sum({total: "total_price"})
+                .groupBy("date")
+                .where(builder => {
+                    builder.whereNull("status_id")
+                        .orWhere("status_id", 16)
+                })
+            if (dateFrom && dateTo)
+                revenueByDay.whereBetween("created_at", [
+                    momentFrom,
+                    momentTo
+                ])
 
-        res.send(response)
+            response.revenueByDay = await revenueByDay
+            response.dateFormat = months < 1 ? "DD MMM" : "MMM YY"
+
+
+            const numberOfPositions = OrderProductColor.query<any>()
+                .count("*", {as: "total"})
+
+            if (dateFrom && dateTo)
+                numberOfPositions.whereBetween("created_at", [
+                    momentFrom,
+                    momentTo
+                ])
+
+            response.numberOfPositions = (await numberOfPositions)[0]?.total || 0
+        } else {
+            const revenueByDay = Order.query<any>()
+                .select(raw(`DATE(created_at) AS date`))
+                .sum({total: "total_price"})
+                .groupBy("date")
+                .where(builder => {
+                    builder.whereNull("status_id")
+                        .orWhere("status_id", 16)
+                })
+            if (dateFrom && dateTo)
+                revenueByDay.whereBetween("created_at", [
+                    momentFrom,
+                    momentTo
+                ])
+
+            response.revenueByDay = await revenueByDay
+            response.dateFormat = months < 1 ? "DD MMM" : "DD MMM YY"
+
+            const numberOfPositions = OrderProductColor.query<any>()
+                .count("*", {as: "total"})
+
+            if (dateFrom && dateTo)
+                numberOfPositions.whereBetween("created_at", [
+                    momentFrom,
+                    momentTo
+                ])
+
+            response.numberOfPositions = (await numberOfPositions)[0]?.total || 0
+        }
+        return res.send(response)
     }
 }
