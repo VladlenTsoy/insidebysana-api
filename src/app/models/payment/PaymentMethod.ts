3382fd320c41e25b02ee3ebdaa5d94e7fcbc674e
vladlengg <ocvbjf5b24aq4snhfnbtlgzuvf4s6vdeygk72cjuxxap3s46o6vq>
2022-07-02 00:06:42 +0500

    update payment methods logos

--- src/app/models/payment/PaymentMethod.ts
+++ src/app/models/payment/PaymentMethod.ts
@@ -3,8 +3,14 @@ import moment from "moment"
 
 export class PaymentMethod extends Model {
     static tableName = "payment_methods"
+    static virtualAttributes = ["url_logo"]
+    logo: string
     created_at: string
     updated_at: string
+
+    url_logo() {
+        if (this.logo) return `${process.env.APP_IMAGE_URL}/${this.logo}`
+    }
 
     $beforeInsert() {
         this.created_at = moment().format("YYYY-MM-DD HH:mm:ss")
